dfe6569a335666b0f16c150816d8e739f1640a3d
Russell Gapasin <russell@fanswifi.com>
2021-11-03 13:02:30 +0800

    Added create_image_url func and fixed exception raise

--- helper.py
+++ helper.py
@@ -1,6 +1,8 @@
 import names
 import random
 from lorem_text import lorem
+from error.exceptions import RequiredParamError, OptionFormatError
+import requests
 
 def generate_fake_data(schema, data_count):
     response = []
@@ -13,7 +15,7 @@ def generate_fake_data(schema, data_count):
     return response
 
 def get_data_cmd_dict():
-    test = {
+    return {
         'first_name': create_first_name,
         'last_name': create_last_name,
         'full_name': create_full_name,
@@ -24,9 +26,9 @@ def get_data_cmd_dict():
         'enum': create_enum,
         'integer': create_integer,
         'boolean': create_boolean,
-        'decimal': create_decimal
+        'decimal': create_decimal,
+        'image_url': create_image_url
     }
-    return test
 
 def generate_fake_value(value):
     data_type = value['type']
@@ -35,19 +37,16 @@ def generate_fake_value(value):
     return cmd_function(options)
 
 def create_first_name(options):
-    if 'gender' in options:
-        gender = options['gender']
-        return names.get_first_name(gender=gender)
-    return names.get_first_name()
+    gender = options['gender'] if 'gender' in options else None
+    return names.get_first_name(gender=gender)
 
 def create_last_name(options):
     return names.get_last_name()
 
 def create_full_name(options):
-    if 'gender' in options:
-        gender = options['gender']
-        return names.get_full_name(gender=gender)
-    return names.get_full_name()
+    first_name = create_first_name(options)
+    last_name = create_last_name(options)
+    return f'{first_name} {last_name}'
 
 def create_email(options):
     gender = options['gender'] if 'gender' in options else None
@@ -67,7 +66,7 @@ def create_paragraph(options):
 
 def create_words(options):
     if 'word_length' not in options:
-        raise MyException('word_length is required')
+        raise RequiredParamError('word_length')
 
     word_length = options['word_length']
     return lorem.words(word_length)
@@ -75,28 +74,28 @@ def create_words(options):
 def create_enum(options):
     # check for required option params
     if 'values' not in options:
-        raise MyException('values is required')
+        raise RequiredParamError('values')
 
     selections = options['values']
 
     # check for option param format
     if not isinstance(selections, list):
-        raise MyException('values type must be an array')
+        raise OptionFormatError('values type must be an array')
 
     return random.choice(selections)
 
 def create_integer(options):
     # check for required option params
     if 'range' not in options:
-        raise MyException('range is required')
+        raise RequiredParamError('range')
 
     range = options['range']
 
     # check for option param format
     if not isinstance(range, list):
-        raise MyException('range type must be an array')
-    if len(range) != 2:
-        raise MyException('range format should be [<min_number>, <max_number>]')
+        raise OptionFormatError('range type must be an array')
+    if len(range) != 2 or range[0] > range[1]:
+        raise OptionFormatError('range format should be [<min_number>, <max_number>]')
 
     min_number = range[0]
     max_number = range[1]
@@ -105,23 +104,39 @@ def create_integer(options):
 def create_decimal(options):
     # check for required option params
     if 'decimal_places' not in options:
-        raise MyException('decimal_places is required')
+        raise RequiredParamError('decimal_places')
     if 'range' not in options:
-        raise MyException('range is required')
+        raise RequiredParamError('range')
 
     decimal_places = options['decimal_places']
     range = options['range']
 
     # check for option param format
     if not isinstance(range, list):
-        raise MyException('range type must be an array')
-    if len(range) != 2:
-        raise MyException('range format should be [<min_number>, <max_number>]')
+        raise OptionFormatError('range type must be an array')
+    if len(range) != 2 or range[0] > range[1]:
+        raise OptionFormatError('range format should be [<min_number>, <max_number>]')
 
     min_number = range[0]
     max_number = range[1]
-    test = round(random.uniform(min_number, max_number), decimal_places)
-    return test
+    return round(random.uniform(min_number, max_number), decimal_places)
 
 def create_boolean(options):
     return bool(random.getrandbits(1))
+
+def create_image_url(options):
+    if 'category' not in options:
+        raise RequiredParamError('category')
+
+    category = options['category']
+    width = options['width'] if 'width' in options else 1080
+
+    if not isinstance(width, int):
+        raise OptionFormatError('width should be an integer')
+
+    r = requests.get(f'https://unsplash.com/napi/search/photos?query={category}&per_page=100')
+    results = r.json()['results']
+
+    random_index = random.randint(0, len(results))
+    image_url = results[random_index]['urls']['regular']
+    return image_url.replace('w=1080', f'w={width}')
